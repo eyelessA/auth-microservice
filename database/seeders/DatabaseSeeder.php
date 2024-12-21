d8ee417cabec1ceff66f0101d352f5c56bfecc55
olegbugaev <bugaev0leg@yandex.ru>
2024-12-21 23:42:41 +0400

    feat: add password

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,6 +17,7 @@ class DatabaseSeeder extends Seeder
 
         User::factory()->create([
             'name' => 'Test User',
+            'password' => 'password',
             'email' => 'test@example.com',
         ]);
     }
